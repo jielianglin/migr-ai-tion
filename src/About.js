b694d32789c8a9424832bbddbf7f0f830637bfc9
gg__X <jielianglin821@gmail.com>
2020-09-05 19:24:08 +0200

    updated About materials

--- src/About.js
+++ src/About.js
@@ -5,16 +5,28 @@ export default class About extends React.Component {
     return (
       <div>
         <p>
-          <b> Jie Liang Lin</b> is an interdisciplinary artist and media
-          anthropologist based in Berlin.
+          <b> Jie Liang Lin (USA)</b> is an interdisciplinary artist and media
+          anthropologist based in Berlin. Her work focuses on the intersections
+          of new technologies and migration phenomena, and the mediation of
+          migration experiences.
           <br />
           <br />
+          <u>Project Consultants: </u>
+          <ul>
+            <li>
+              Joanna Sleigh (CH) on <i>Data Visualisation:</i> is a visual and
+              media anthropologist and Phd candidate at the ETH Zürich, Health
+              Ethics and Policy Lab. Her work focuses on visualisation of health
+              policy communication.
+            </li>
+            <li>
+              Peggy Sylopp (DE) on <i>Computer Science:</i> is an
+              interdisciplinary artist and computer scientist. She is project
+              manager of her citizens' project "Hear How You Like to Hear" at
+              Frauenhofer IDMT Oldenburg.
+            </li>
+          </ul>
           <br />
-          <u>Project Consultants: </u> <br />
-          <br />
-          <b> Joanna Sleigh</b> on <i>Data Visualisation</i>
-          <br />
-          <b>Peggy Sylopp</b> on <i>Computer Science</i>
         </p>
       </div>
     );
