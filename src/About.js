a4354491b4d0699c94eff59ef354324bcac31755
gg__X <jielianglin821@gmail.com>
2020-09-05 19:38:16 +0200

    installed bootstrap and updated about materials

--- src/About.js
+++ src/About.js
@@ -1,26 +1,31 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default class About extends React.Component {
   render() {
     return (
-      <div>
+      <div className="col-6">
         <p>
-          <b> Jie Liang Lin (USA)</b> is an interdisciplinary artist and media
-          anthropologist based in Berlin. Her work focuses on the intersections
-          of new technologies and migration phenomena, and the mediation of
-          migration experiences.
+          <b> Jie Liang Lin (USA):</b>
+          <br /> is an interdisciplinary artist and media anthropologist based
+          in Berlin. Her work focuses on the intersections of new technologies
+          and migration phenomena, and the mediation of migration experiences.
           <br />
           <br />
-          <u>Project Consultants: </u>
+          <u>
+            <i>Consultants: </i>
+          </u>
+          <br />
           <ul>
             <li>
-              Joanna Sleigh (CH) on <i>Data Visualisation:</i> is a visual and
-              media anthropologist and Phd candidate at the ETH Zürich, Health
-              Ethics and Policy Lab. Her work focuses on visualisation of health
-              policy communication.
+              <b> Joanna Sleigh (CH)</b> on <u>Data Visualisation:</u> <br />
+              is a visual and media anthropologist and Phd candidate at the ETH
+              Zürich, Health Ethics and Policy Lab. Her work focuses on
+              visualisation of health policy communication.
             </li>
+            <br />
             <li>
-              Peggy Sylopp (DE) on <i>Computer Science:</i> is an
+              <b>Peggy Sylopp (DE) </b> on <u>Computer Science:</u> <br /> is an
               interdisciplinary artist and computer scientist. She is project
               manager of her citizens' project "Hear How You Like to Hear" at
               Frauenhofer IDMT Oldenburg.
